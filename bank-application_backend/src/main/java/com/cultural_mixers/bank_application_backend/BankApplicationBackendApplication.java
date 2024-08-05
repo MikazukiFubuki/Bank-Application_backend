e7e27f9e14322098f0d51ebd4d82296029559ae3
IkkiKyomoto <kazu06851@gmail.com>
2024-08-05 15:37:01 +0900

    first commit

---
+++ bank-application_backend/src/main/java/com/cultural_mixers/bank_application_backend/BankApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cultural_mixers.bank_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ bank-application_backend/src/test/java/com/cultural_mixers/bank_application_backend/BankApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultural_mixers.bank_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
