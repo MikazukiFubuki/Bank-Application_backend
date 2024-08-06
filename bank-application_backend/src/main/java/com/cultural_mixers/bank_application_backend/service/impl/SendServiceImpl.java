8643b25e4eaa5c6a9ae65b5b176974d8448e99ba
fubuki <wyx1337189880@gmail.com>
2024-08-06 12:32:45 +0900

    08/06 12:32

--- bank-application_backend/src/main/java/com/cultural_mixers/bank_application_backend/service/impl/SendServiceImpl.java
+++ bank-application_backend/src/main/java/com/cultural_mixers/bank_application_backend/service/impl/SendServiceImpl.java
@@ -1,11 +1,16 @@
-package main.java.com.cultural_mixers.bank_application_backend.service.impl;
+package com.cultural_mixers.bank_application_backend.service.impl;
+
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cultural_mixers.bank_application_backend.dao.SendDao;
 import com.cultural_mixers.bank_application_backend.dao.UserDao;
+import com.cultural_mixers.bank_application_backend.domain.Send;
 import com.cultural_mixers.bank_application_backend.domain.User;
+import com.cultural_mixers.bank_application_backend.service.SendService;
 import com.cultural_mixers.bank_application_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,21 +20,24 @@ public class SendServiceImpl extends ServiceImpl<SendDao, Send> implements SendS
     @Autowired
     private SendDao sendDao;
 
+    @Autowired
+    private UserDao userDao;
+
     @Override
     public boolean sendMoney(Send send) {
         try {
 
-            
-            double balance = sendDao.selectBalance(send.getSe_user_id());
+            User user = userDao.selectBalance(send.getSe_user_id());
+            double balance = user.getBalance();
             if (balance < send.getMoney()) {
                 return false;
             }
             sendDao.sendMoney(send);
             sendDao.receiveMoney(send);
             sendDao.sendRecords(send);
-            return true
+            return true;
         } catch (Exception e) {
-            return false
+            return false;
         }
 
     // @Override
